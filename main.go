b1895f837cb201adc839fb35ed88dc46f6846b0c
melisebestrada <sebastian.estrada@mercadolibre.com.co>
2025-01-14 18:04:09 -0500

    feat: getProductsData from json file created

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/melisebestrada/go-web-api/products"
+)
+
+func main() {
+	products, err := getProductsData()
+	if err != nil {
+		fmt.Println("error getting products data: ", err)
+		return
+	}
+
+	fmt.Println(products)
+}
+
+func getProductsData() ([]products.Product, error) {
+	file, err := os.Open("data/products.json")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var products []products.Product
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&products)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
